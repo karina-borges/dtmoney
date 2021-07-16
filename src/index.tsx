3d08f261d48df714c22a7ab82bc208d153c214fd
Karina Borges <karina@cuponeria.com.br>
2021-07-15 21:20:25 -0300

    ✨ Creating Transaction Context

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "freelancer",
+          type: "deposit",
+          category: "dev",
+          amount: 1000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "aluguel",
+          type: "withdraw",
+          category: "casa",
+          amount: 1300,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
